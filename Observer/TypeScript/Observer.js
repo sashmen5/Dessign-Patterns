62187219db71fbae8be3df90a3f0d712841f13a9
sashmen5 <sashmen5@gmail.com>
2018-12-24 16:58:05 +0200

    Typescripts observer created

---
+++ Observer/TypeScript/Observer.js
@@ -0,0 +1,53 @@
+var WeatherStation = (function () {
+    function WeatherStation() {
+        this.observers = [];
+    }
+    WeatherStation.prototype.setTemperature = function (temp) {
+        console.log('WeatherStation: new temperature measurement: ' + temp);
+        this.temperature = temp;
+        this.notifyObservers();
+    };
+    WeatherStation.prototype.registerObserver = function (o) {
+        this.observers.push(o);
+    };
+    WeatherStation.prototype.removeObserver = function (o) {
+        var index = this.observers.indexOf(o);
+        if (index !== -1) {
+            this.observers.splice(index, 1);
+        }
+    };
+    WeatherStation.prototype.notifyObservers = function () {
+        for (var _i = 0, _a = this.observers; _i < _a.length; _i++) {
+            var observer = _a[_i];
+            observer.update(this.temperature);
+        }
+    };
+    return WeatherStation;
+}());
+var TemperatureDisplay = (function () {
+    function TemperatureDisplay(weatherStation) {
+        this.subject = weatherStation;
+        weatherStation.registerObserver(this);
+    }
+    TemperatureDisplay.prototype.update = function (temperature) {
+        console.log('TemperatureDisplay: I need to update mu display.');
+    };
+    return TemperatureDisplay;
+}());
+var Fan = (function () {
+    function Fan(weatherStation) {
+        this.subject = weatherStation;
+        weatherStation.registerObserver(this);
+    }
+    Fan.prototype.update = function (temperature) {
+        if (temperature > 25) {
+            console.log('Fan: It is hot here, turning myself on...');
+            // Here some real logic
+        }
+        else {
+            console.log('Fan: It is nice and cool, turning myself off...');
+            // Here some real logic
+        }
+    };
+    return Fan;
+}());
---
+++ Observer/TypeScript/Observer.ts
@@ -0,0 +1,67 @@
+interface Subject {
+    registerObserver(o: Observer);
+    removeObserver(o: Observer);
+    notifyObservers();
+}
+
+
+interface Observer {
+    update(temperature: number);
+}
+
+class WeatherStation implements Subject{
+    private temperature: number;
+    private observers: Observer[] = [];
+    setTemperature(temp: number): void {
+        console.log('WeatherStation: new temperature measurement: ' + temp);
+        this.temperature = temp;
+        this.notifyObservers();
+    }
+
+    registerObserver(o: Observer) {
+        this.observers.push(o);
+    }
+
+    removeObserver(o: Observer) {
+        let index = this.observers.indexOf(o);
+        if (index !== -1) {
+            this.observers.splice(index, 1);
+        }
+    }
+
+    notifyObservers() {
+        for (const observer of this.observers) {
+            observer.update(this.temperature);
+        }
+    }
+}
+
+class TemperatureDisplay implements Observer {
+    private subject: Subject;
+    constructor(weatherStation: Subject) {
+        this.subject = weatherStation;
+        weatherStation.registerObserver(this);
+    }
+
+    update(temperature: number) {
+        console.log('TemperatureDisplay: I need to update mu display.');
+    }
+}
+
+class Fan implements Observer {
+    private subject: Subject;
+    constructor(weatherStation: Subject) {
+        this.subject = weatherStation;
+        weatherStation.registerObserver(this);
+    }
+
+    update(temperature: number) {
+        if (temperature > 25) {
+            console.log('Fan: It is hot here, turning myself on...');
+            // Here some real logic
+        } else {
+            console.log('Fan: It is nice and cool, turning myself off...');
+            // Here some real logic
+        }
+    }
+}
